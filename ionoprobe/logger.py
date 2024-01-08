1322ec7afddaafd52d47b5207ef3706c641a4d60
javicebri <jac.cebrian@gmail.com>
2024-01-08 10:15:36 +0100

    Add logger with seconds and date in the file name

--- ionoprobe/logger.py
+++ ionoprobe/logger.py
@@ -1,4 +1,5 @@
 import logging
+import datetime
 import logging.handlers
 import sys
 import os
@@ -6,7 +7,9 @@ from logging import Logger
 
 
 def init_logger(root, exec_str):
-    logger_path = os.path.join(root, 'logs', 'log_' + exec_str + '.log')
+    date = datetime.datetime.now()
+    date_str = date.strftime("%Y%m%d_%H%M%S")
+    logger_path = os.path.join(root, 'logs', 'log_' + exec_str + '_' + date_str + '.log')
     if not os.path.exists(logger_path):
         with open(logger_path, 'w') as f:
             f.write("")
@@ -15,7 +18,7 @@ def init_logger(root, exec_str):
     log.setLevel(logging.INFO)
     handler = logging.FileHandler(logger_path)
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s", "%Y-%m-%d %H:%M")
+    formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s", "%Y-%m-%d %H:%M:%S")
     handler.setFormatter(formatter)
     log.addHandler(handler)
 
