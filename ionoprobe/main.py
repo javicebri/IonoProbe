3efa729d595a483ad0be4daf608fa4fc7da2e246
javicebri <jac.cebrian@gmail.com>
2024-01-08 10:00:02 +0100

    Add log infor in main

--- ionoprobe/main.py
+++ ionoprobe/main.py
@@ -4,6 +4,7 @@ import os
 import json
 import requests
 import logging
+import GLOBAL_VARS
 from logger import logger, init_logger
 from paths import get_paths, create_paths, add_config_paths
 from config import load_config, format_config
@@ -14,10 +15,13 @@ def main(root="/"):
     # Create paths dict
     paths_dict = get_paths(root)
     create_paths(paths_dict)
-    logger = init_logger(root, 'inoprobe')
+    logger = init_logger(root, 'ionoprobe')
+    logger.info(GLOBAL_VARS.header_art)
 
     # Read Config
+    logger.info('Load config.')
     config = load_config(paths_dict)
+    logger.info('Format config.')
     config = format_config(config)
 
     # Add paths
@@ -27,10 +31,11 @@ def main(root="/"):
     run_steps = config['RUN']
 
     for step_i in run_steps:
-        # logger.info('Start GOES Download.')
         if step_i == 'Download_GOES':
+            logger.info('Start GOES Download.')
             download_GOES(paths_dict, config)
         elif step_i == 'Download_DIGISONDE':
+            logger.info('Start DIGISONSE Download.')
             download_digisonde(paths_dict, config)
 
 main(root = '/home/javier/Projects/IonoProbe/')
